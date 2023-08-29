05e501b9d2bdd47548ee0dc41bdebda7b18ee5f9
rafhael <rafhaelmilanes12@gmail.com>
2023-08-28 21:29:11 -0300

    codigo aula 02

---
+++ aulas/aula02/App.js
@@ -0,0 +1,24 @@
+import { View, StyleSheet } from 'react-native';
+import Box from './components/Box';
+
+//componente principal do App
+
+const App = () => (
+  <View style={styles.container}>
+    <Box size={50} color="blue"></Box>
+    <Box size={50} color="red"></Box>
+    <Box size={50} color="yellow"></Box>
+  </View>
+);
+
+//estilo objeto StyleSheet
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+});
+
+export default App;
